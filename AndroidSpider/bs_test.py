40529ec2cf44d64e76e5a64e170588d769341dd9
Wei.Guang <yemingrujing@163.com>
2019-08-22 01:14:16 +0800

    scrapy

--- AndroidSpider/bs_test.py
+++ AndroidSpider/bs_test.py
@@ -23,6 +23,7 @@ ua_list = [
     "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"
 ]
+
 etree = html.etree
 userAgent = random.choice(ua_list)
 
